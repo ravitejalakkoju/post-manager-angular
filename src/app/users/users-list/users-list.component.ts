57a86d783752b20fe2155bf85ee8f7db3bea1b0d
listings <educative@example.com>
2023-03-28 20:02:00 +0530

    feature(sort users): added sort functionality to users list

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { User } from '../../models/user';
 
@@ -13,4 +13,30 @@ import { ChangeTrackerService } from '../../services/change-tracker.service';
 export class UsersListComponent {
   @Input() users: User[];
   @Input() activeId: number;
+
+  usersList: User[];
+  isListSorted: boolean = false;
+
+  ngOnInit() {
+    this.usersList = this.users.map(u => u);
+  }
+
+  compareByName(user1: User, user2: User) {
+    if (user1.name < user2.name) {
+      return -1;
+    }
+    if (user1.name > user2.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  toggleSort() {
+    // this.sortUsersList.emit(true);
+    if(!this.isListSorted)
+      this.usersList.sort(this.compareByName);
+    else 
+      this.usersList = this.users.map(u => u);
+    this.isListSorted = !this.isListSorted;
+  }
 }
