7d84cd77bddf55adad96e2f9558f692a5b302570
listings <educative@example.com>
2023-03-28 20:22:03 +0530

    ui: added a-z and z-a sort

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -14,20 +14,20 @@ export class UsersListComponent {
   @Input() users: User[];
   @Input() activeId: number;
 
-  isListSorted: boolean = false;
+  isAZSorted: boolean = false;
 
   compareByName(user1: User, user2: User) {
     if (user1.name < user2.name) {
-      return -1;
+      return this.isAZSorted ? 1 : -1;
     }
     if (user1.name > user2.name) {
-      return 1;
+      return this.isAZSorted ? - 1 : 1;
     }
     return 0;
   }
 
   sortUsers() {
-    this.users.sort(this.compareByName);
-    this.isListSorted = true;
+    this.users.sort(this.compareByName.bind(this));
+    this.isAZSorted = !this.isAZSorted;
   }
 }
